34370d0b94e0181c1d209d5b055a5b69faf0ec27
Yaswanth <134692338+Jaladi-Yaswanth@users.noreply.github.com>
2024-09-10 16:18:25 +0530

    Add files via upload

---
+++ 169.Majority_Element.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+class Solution {
+
+    public int majorityElement(int[] nums) {
+      int count=0;
+
+      int read=nums[0];
+
+      Map<Integer,Integer> map= new HashMap<>();
+
+      for(int i =0;i<nums.length;i++){
+        if(map.containsKey(nums[i])) map.put(nums[i],(map.get(nums[i])+1));
+
+        else map.put(nums[i],1);
+      }
+
+     int n=nums.length/2;
+
+      for(Map.Entry<Integer,Integer> mapp : map.entrySet()) {
+        if(mapp.getValue()>n) return mapp.getKey();
+      }
+      
+    return 0;
+    }
+}
